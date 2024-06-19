d4d68903da9ad67a82c4263d73ceb6affabe9b7b
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-06-19 06:52:41 +0530

    added ref

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GoBellFill } from "react-icons/go";
 import { HiMiniBars3 } from "react-icons/hi2";
 import { HiX } from "react-icons/hi";
-
+import {AuthContext} from '../context/AuthContext'
 
 
 const Navbar = ({ toggleSidebar, isSidebarVisible = false }) => {
+
+    const { userData } = useContext(AuthContext);
+
     return (
         <>
             <div className='bg-shadeWhite w-[100%] h-[8vh] flex items-center xs:px-4 lg:px-8 '>
@@ -23,7 +26,9 @@ const Navbar = ({ toggleSidebar, isSidebarVisible = false }) => {
                         <span className='ping xs:text-sm'></span>
                     </div>
                     <div className='bg-grey xs:h-[30px] xs:w-[30px] lg:h-[35px] lg:w-[35px] rounded-[100%] ml-6 grid place-content-center'>G</div>
-                    {/* <div className='h-[300px] w-[250px] absolute right-5 top-16 border-2 border-grey rounded-xl bg-white'></div> */}
+                    {/* <div className='h-[300px] w-[250px] absolute right-5 top-16 border-2 border-grey rounded-xl bg-white'>
+
+                    </div> */}
                 </div>
 
             </div>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,20 +1,37 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useRef } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState(false);
-    const [userData, setUserData] = useState(null);
+    const [userData, setUserDataState] = useState(null);
+    const userDataRef = useRef(null);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-        if (token) {
-            setAuth(true);  
+        const storedUserData = localStorage.getItem('userData');
+        if (token && storedUserData) {
+            setAuth(true);
+            const parsedUserData = JSON.parse(storedUserData);
+            setUserDataState(parsedUserData);
+            userDataRef.current = parsedUserData;
         }
     }, []);
 
+    useEffect(() => {
+        if (userData) {
+            localStorage.setItem('userData', JSON.stringify(userData));
+            userDataRef.current = userData;
+        }
+    }, [userData]);
+
+    const setUserData = (data) => {
+        setUserDataState(data);
+        userDataRef.current = data;
+    };
+
     return (
-        <AuthContext.Provider value={{ auth, setAuth, userData, setUserData }}>
+        <AuthContext.Provider value={{ auth, setAuth, userData, setUserData, userDataRef }}>
             {children}
         </AuthContext.Provider>
     );
