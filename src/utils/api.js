4dcc69eb3cf7a9438a1e4f151f6cf63036ae489c
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-07-03 23:04:29 +0530

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,8 @@ const api = axios.create({
   // withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
-    "Access-Control-Allow-Origin": "*"
+    // "Access-Control-Allow-Origin": "*"
+    // "Access-Control-Allow-Headers": "Content-Type, Authorization"
   },
 });
 
