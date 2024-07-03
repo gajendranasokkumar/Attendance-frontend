f4f21fb6eb4189ad5575671ed460f0de3cf4cd96
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-07-03 22:36:16 +0530

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,18 +2,31 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: `${import.meta.env.VITE_BASE_URL}`,
+  baseURL: import.meta.env.VITE_BASE_URL, // Ensure this environment variable is correctly set
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers["Authorization"] = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   },
