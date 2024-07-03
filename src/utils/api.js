e79ff96e8b144019f16d49624e88aa3c2927e4bc
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-07-03 21:58:58 +0530

    updated api

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}`,
   withCredentials: true,
   headers: {
-    "Access-Control-Allow-Origin": "*",
+    'Content-Type': 'application/json',
   },
 });
 
