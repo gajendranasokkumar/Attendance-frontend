065521d82d8415f5afbd8ab79d19c5cb193613d0
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-07-03 23:13:54 +0530

    made some changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -29,6 +29,7 @@ api.interceptors.request.use(
     const token = localStorage.getItem('token');
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['Credentials'] = true;
     }
     return config;
   },
