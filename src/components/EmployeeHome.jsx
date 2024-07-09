eb25a2073baa001318a7ebd6da16ff170bffd75a
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-07-09 22:21:32 +0530

    updated the sidebar visibility

--- src/components/EmployeeHome.jsx
+++ src/components/EmployeeHome.jsx
@@ -1,24 +1,77 @@
-import React, { useContext, useState } from 'react'
+// import React, { useContext, useState } from 'react'
+// import Navbar from './Navbar'
+// import EmpSidebar from './EmpSidebar'
+// import EmpContent from './EmpContent'
+// import LeaveList from './LeaveList'
+// import { Outlet } from 'react-router-dom'
+// import { AuthContext } from '../context/AuthContext';
+
+
+// const EmployeeHome = () => {
+//     const [isSidebarVisible, setSidebarVisible] = useState(false);
+//     const { userData } = useContext(AuthContext);
+
+
+//     const toggleSidebar = () => {
+//         setSidebarVisible(!isSidebarVisible);
+//     };
+
+//     return (
+//         <>
+//         {console.log("🚀 ~ EmployeeHome ~ userData:", userData)}
+            
+//             <Navbar toggleSidebar={toggleSidebar} isSidebarVisible={isSidebarVisible} />
+//             <div className="flex w-[100vw]">
+//                 <div className={`lg:inline-block ${isSidebarVisible ? 'absolute' : 'hidden'} xs:w-[60%] lg:w-[18%] z-[999]`}>
+//                     <EmpSidebar />
+//                 </div>
+//                 <div className='w-[100%] '>
+//                     {/* <EmpContent />
+//                     <LeaveList /> */}
+//                     <Outlet />
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default EmployeeHome
+
+
+
+
+
+import React, { useContext, useState, useEffect } from 'react'
 import Navbar from './Navbar'
 import EmpSidebar from './EmpSidebar'
-import EmpContent from './EmpContent'
-import LeaveList from './LeaveList'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext';
-
 
 const EmployeeHome = () => {
     const [isSidebarVisible, setSidebarVisible] = useState(false);
     const { userData } = useContext(AuthContext);
+    const location = useLocation();
 
+    useEffect(() => {
+        const handleRouteChange = () => {
+            if (window.innerWidth < 640) {
+                setSidebarVisible(false);
+            }
+        };
+
+        handleRouteChange(); 
+
+    }, [location]); 
 
     const toggleSidebar = () => {
-        setSidebarVisible(!isSidebarVisible);
+        if (window.innerWidth < 1024) {
+            setSidebarVisible(!isSidebarVisible);
+        }
     };
 
     return (
         <>
-        {console.log("🚀 ~ EmployeeHome ~ userData:", userData)}
+            {console.log("🚀 ~ EmployeeHome ~ userData:", userData)}
             
             <Navbar toggleSidebar={toggleSidebar} isSidebarVisible={isSidebarVisible} />
             <div className="flex w-[100vw]">
@@ -26,8 +79,6 @@ const EmployeeHome = () => {
                     <EmpSidebar />
                 </div>
                 <div className='w-[100%] '>
-                    {/* <EmpContent />
-                    <LeaveList /> */}
                     <Outlet />
                 </div>
             </div>
